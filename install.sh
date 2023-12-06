5a40770cd6566fcdae6761c4f3db6d54a4345416
Koen Kist (PhoenixStarYT) <104870072+PhoenixStarYT@users.noreply.github.com>
2023-12-06 15:12:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,42 +16,51 @@ echo -ne "
 ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝
 "
 
-#Paru AUR Helper
-sudo pacman --noconfirm -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+#Yay AUR Helper
+pacman --noconfirm -S --needed base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
 makepkg -si
 
 #Update and install Essential Packages
-sudo pacman --noconfirm -Syu
-sudo pacman --noconfirm -S neofetch
-sudo pacman --noconfirm -S htop
-sudo pacman --noconfirm -S bspwm
-sudo pacman --noconfirm -S sxhkd
-sudo pacman --noconfirm -S polybar
-sudo pacman --noconfirm -S rofi
-sudo pacman --noconfirm -S picom
-sudo pacman --noconfirm -S git
-sudo pacman --noconfirm -S kitty
-sudo pacman --noconfirm -S wget
-sudo pacman --noconfirm -S unzip
-sudo pacman --noconfirm -S nano
-sudo pacman --noconfirm -S plasma
-sudo pacman --noconfirm -S awesome
-
-#Install Desktop apps
-sudo pacman --noconfirm -S nautilus
-sudo pacman --noconfirm -S nitrogen
-sudo pacman --noconfirm -S variety
-sudo pacman --noconfirm -S lxappearance
-sudo pacman --noconfirm -S pulseaudio
-sudo pacman --noconfirm -S pavucontrol
-sudo pacman --noconfirm -S sddm
-sudo pacman --noconfirm -S rofi
-sudo pacman --noconfirm -S firefox
+yay --no-confirm -Syu
+yay --no-confirm -S vim unzip picom bspwm awesome openbox polybar lxsession lxpanel lightdm rofi kitty terminator thunar flameshot neofetch sxhkd git lxpolkit lxappearance xorg firefox-esr pulseaudio pavucontrol
+yay --no-confirm -S tar papirus-icon-theme nitrogen lxappearance breeze noto-color-emoji-fontconfig fonts-firacode font-awesome libqt5svg5 qml-module-qtquick-controls qml-module-qtquick-controls2 variety ocs-url
 
 #Setup SDDM
-sudo systemctl enable sddm
+systemctl enable sddm
+
+#Theming & Fonts
+yay --no-confirm -S xfce-theme-greybird
+yay --no-confirm -S bibata-cursor-theme
+yay --no-confirm -S breeze
+yay --no -confirm -S otf-font-awesome
+yay --no-confirm -S ttf-font-awesome
+yay --no-confirm -S zorin-themes-desktop
+yay --no-confirm -S tela-icon-theme
+
+cd 
+mkdir .config
+cd koenarch-bspwm
+mv dotconfig/* ~/.config
+mv .* ~
+
+# Download Nordic Theme
+cd /usr/share/themes/
+git clone https://github.com/EliverLara/Nordic.git
+
+# Fira Code Nerd Font variant needed
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/FiraCode.zip
+unzip FiraCode.zip -d /usr/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/Meslo.zip
+unzip Meslo.zip -d /usr/share/fonts
+fc-cache -vf
 
 echo -ne "
 ██████╗ ███████╗██╗   ██╗████████╗
