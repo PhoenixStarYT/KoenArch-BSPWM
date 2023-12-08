791a12b2bf06246507be90aaf1fdd598d06dba5b
phoenixstaryt <koenkist18@gmail.com>
2023-12-08 15:47:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,14 +16,9 @@ echo -ne "
 ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝
 "
 
-# Check if Script is Run as Root
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
-  exit 1
-fi
 
 #Yay AUR Helper
-pacman --noconfirm -S --needed base-devel
+sudo pacman --noconfirm -S --needed base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
